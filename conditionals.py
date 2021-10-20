0ff8161adb90aae1f0d0c04620aa9fd7bf566aab
Meg <meganriley806@gmail.com>
2021-10-19 20:12:14 -0500

    removed some comments

--- conditionals.py
+++ conditionals.py
@@ -23,23 +23,7 @@ def reactor_efficiency(voltage, current, theoretical_max_power):
 
 
 def fail_safe(temperature, neutrons_produced_per_second, threshold):
-    """Assess and return status code for the reactor.
-
-    :param temperature: value of the temperature (integer or float)
-    :param neutrons_produced_per_second: neutron flux (integer or float)
-    :param threshold: threshold (integer or float)
-    :return: str one of: 'LOW', 'NORMAL', 'DANGER'
-
-    - `temperature * neutrons per second` < 90% of `threshold` == 'LOW'
-    - `temperature * neutrons per second` +/- 10% of `threshold` == 'NORMAL'
-    - `temperature * neutrons per second` is not in the above-stated ranges ==  'DANGER'
-    """
-
     value = temperature * neutrons_produced_per_second
-    minus_ten = threshold - (threshold * .10)
-    print(minus_ten)
-    plus_ten = threshold + (threshold * .10)
-    print(plus_ten)
     if value < (.9 * threshold):
         return 'LOW'
     elif value >= (.9 * threshold) and value <= (1.1 * threshold):
